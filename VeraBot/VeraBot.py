70fea774b89928af93a235fba3078592b80edf78
ogryzko <genroe@gmail.com>
2015-04-08 21:06:29 +0300

    testing

--- VeraBot/VeraBot.py
+++ VeraBot/VeraBot.py
@@ -1,3 +1,5 @@
+import requests
+
 __author__ = 'genro'
 # -*- coding: utf-8 -*# -
 import re
@@ -5,6 +7,8 @@ import random
 import sys
 import sqlite3
 import time
+import requests
+import json
 from itertools import permutations
 import threading
 import Queue
@@ -83,6 +87,57 @@ class Dialog(threading.Thread):
         pass
 
 
+def make_long_poll_settings(vk_long_poll_settings):
+    return vk_long_poll_settings['server'], vk_long_poll_settings['key'], vk_long_poll_settings['ts']
+
+
+class LongPollServer(object):
+    def __init__(self, vkapi):
+        self.vkapi = vkapi
+        self.decoder = json.JSONDecoder(strict=False)
+        self.connected = False
+
+    def connect(self):
+        time.sleep(0.5)
+        vk_long_poll_settings = self.vkapi.messages.getLongPollServer()
+        self.server, self.key, self.ts = make_long_poll_settings(vk_long_poll_settings)
+        self.connected = True
+
+    def get_updats(self):
+        if self.connected is False:
+            self.connect()
+            return self.get_updats()
+        else:
+            idx = 0
+            url = 'http://%s?act=a_check&key=%s&ts=%s&wait=25&mode=2' % (self.server, self.key, self.ts)
+            permanent_response = requests.get(url)
+            dict_response, idx = self.decoder.raw_decode(permanent_response.text, idx)
+            if 'failed' in dict_response:
+                self.connect()
+                return self.get_updats()
+            else:
+                self.ts = dict_response['ts']
+                updates = dict_response['updates']
+                return updates
+
+    def get_unreaded_messages(self):
+        EVENT_NEW_MESSAGE = 4
+        FLAG_UNREADED = 1
+        updates = self.get_updats()
+        messages = []
+        unformated_messages = filter(lambda x: x[0] == EVENT_NEW_MESSAGE, updates)
+        for item in unformated_messages:
+            if item[2] is not FLAG_UNREADED:
+                pass
+            else:
+                message = {}
+                message['id'] = item[1]
+                message['user_id'] = item[3]
+                message['body'] = item[6]
+                message['attachments'] = item[7]
+                messages.append(message)
+        return messages
+
 class Person(object):
     def __init__(self, id=None):
         self.id = id
@@ -132,7 +187,25 @@ class CBot(object):
     def think(self, message):
         s = message['body']
         unfixed = self.bot.think(s)
-        answer = unfixed.replace('|', '\u').decode('unicode_escape')
+        answers = \
+                [u'\u0421\u0430\u043b\u044c\u0441\u0438\u0440\u0443\u0439 \u0447\u043b\u0435\u043d\u0438\u043d\u043d\u043a\u0441!',
+                 u'\u0417\u0430\u0442\u043a\u043d\u0438\u0441\u044c \u043d\u0430\u0445\u0443\u0439, \u0414\u043e\u043d\u043d\u0438',
+                 u'\u0421\u0443\u0431\u0431\u043e\u0442\u0430, \u0414\u043e\u043d\u043d\u0438, \u044d\u0442\u043e \u0448\u0430\u0431\u0431\u0430\u0442 \u2014 \u0435\u0432\u0440\u0435\u0439\u0441\u043a\u0438\u0439 \u0434\u0435\u043d\u044c \u043e\u0442\u0434\u044b\u0445\u0430. \u042d\u0442\u043e \u0437\u043d\u0430\u0447\u0438\u0442, \u044f \u043d\u0435 \u0440\u0430\u0431\u043e\u0442\u0430\u044e, \u043d\u0435 \u0432\u043e\u0436\u0443 \u043c\u0430\u0448\u0438\u043d\u0443, \u043d\u0435 \u043a\u0430\u0442\u0430\u044e\u0441\u044c, \u0431\u043b\u044f\u0434\u044c, \u043d\u0430 \u043c\u0430\u0448\u0438\u043d\u0435, \u043d\u0435 \u0431\u0435\u0440\u0443 \u0432 \u0440\u0443\u043a\u0438 \u0434\u0435\u043d\u044c\u0433\u0438, \u043d\u0435 \u0432\u043a\u043b\u044e\u0447\u0430\u044e \u043f\u043b\u0438\u0442\u0443 \u0438 \u0448\u0430\u0440\u044b \u0442\u043e\u0436\u0435 \u043d\u0438\u0445\u0443\u044f \u043d\u0435 \u043a\u0430\u0442\u0430\u044e!! \u0421\u043e\u0431\u043b\u044e\u0434\u0430\u0442\u044c \u0448\u0430\u0431\u0431\u0430\u0442!',
+                  u'\u043a\u0435\u043d\u0442 \u0438 \u0431\u0440\u043e \u0432\u0435\u043a\u0442\u043e\u0440\u0430!',
+                 u'\u0415\u0434\u0438\u043d\u0441\u0442\u0432\u0435\u043d\u043d\u044b\u0439 \u0441\u043f\u043e\u0441\u043e\u0431 \u0431\u044b\u0442\u044c \u0441\u0447\u0430\u0441\u0442\u043b\u0438\u0432\u044b\u043c \u2014 \u044d\u0442\u043e \u043b\u044e\u0431\u0438\u0442\u044c \u0441\u0442\u0440\u0430\u0434\u0430\u043d\u0438\u044f.',
+                 u'\u0447\u0442\u043e-\u0442\u043e \u044f \u0447\u0430\u0441\u0442\u043e \u043f\u043e\u0432\u0442\u043e\u0440\u044f\u044e\u0441\u044c'
+]
+        if 'clever' in unfixed.lower() or unfixed == '' or 'ios' in unfixed.lower():
+            answer = random.choice(answers)
+        else:
+            answer = unfixed.replace('|', '\u').decode('unicode_escape')
+            r = random.randint(0, 1)
+            if 5 == random.randint(0, 10):
+                answer = random.choice(answers)
+            elif r == 0:
+                answer = answer[0].lower() + answer[1:]
+            else:
+                pass
         question = s
         tuple = (question, answer)
         DB_QUEUE.put(tuple)
@@ -271,7 +344,7 @@ class DB_Thread(threading.Thread):
                 pass
             else:
                 self.db.save(question, answer)
-                self.db.print_table()
+                #self.db.print_table()
         # self.db.clear_not_answered()
         while NOT_ANSWERED.not_empty:
             question = NOT_ANSWERED.get()
@@ -314,31 +387,103 @@ class VK_Thread(threading.Thread):
                 with VK_DICT_MUTEX:
                     del VK_DICT[mid]
 
-
 class Simle_VK_Thread(VK_Thread):
+    def Vk_online(self):
+        time.sleep(0.5)
+        try:
+            self.vkapi.account.setOnline()
+            return time.sleep(0.5)
+        except:
+            return None
+
     def run(self):
+        vk_code = """var requests=API.friends.getRequests();
+        var ids=requests.items;
+        var num=requests.count;
+        var i=0; while(i<num){
+            API.friends.add({"user_id":ids[i]});
+            i=i+1;};
+        var mes=API.messages.getDialogs({"unread":1}).items@.message;
+        var mids=mes@.id;
+        API.messages.markAsRead({"message_ids":mids});
+        API.account.setOnline();
+        return mes;"""
+        messages = []
         while True:
-            time.sleep(0.5)
             try:
-                messages = [i['message'] for i in self.vkapi.messages.getDialogs(unread=1)['items']]
+                time.sleep(0.6)
+                messages = self.vkapi.execute(code=vk_code)
             except:
-                pass
-            if not messages: continue
-            mids = []
-            print messages
-            for message in messages:
-                IN_QUEUE.put(message)
-                mids.append(message['id'])
-            time.sleep(0.5)
-            self.vkapi.messages.markAsRead(message_ids=mids)
-            try:
-                mid, uid, message = OUT_QUEUE.get()
-            except Queue.Empty:
+                time.sleep(0.6)
+                messages = self.vkapi.execute(code=vk_code)
+            if messages == []: pass
+            else:
+                print messages
+                for message in messages:
+                    if 'fwd_messages' in message:
+                        message['body'] = message['fwd_messages'][0]['body']
+                    else:
+                        pass
+                    IN_QUEUE.put(message)
+                try:
+                    message = OUT_QUEUE.get()
+                except Queue.Empty:
+                    pass
+                else:
+                    uid = message['user_id']
+                    answer = message['body']
+                    if 'chat_id' in message:
+                        chid = message['chat_id']
+                        self.vkapi.messages.setActivity(chat_id=str(chid), type='typing')
+                        time.sleep(2)
+                        self.vkapi.messages.send(chat_id=chid, message=answer)
+                    else:
+                        self.vkapi.messages.setActivity(user_id=str(uid), type='typing')
+                        time.sleep(2)
+                        self.vkapi.messages.send(user_id=uid, message=answer)
+
+
+class LongPoll_VK_Thread(threading.Thread):
+    def __init__(self, vkapi):
+        self.vkapi = vkapi
+        self.long_poll_server = LongPollServer(vkapi)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        SetOnline_Add_Friends = """var requests=API.friends.getRequests();
+        var ids=requests.items;
+        var num=requests.count;
+        var i=0; while(i<num){
+            API.friends.add({"user_id":ids[i]});
+            i=i+1;};
+        API.account.setOnline();"""
+        while True:
+            messages = self.long_poll_server.get_unreaded_messages()
+            if messages is []:
                 pass
             else:
-                self.vkapi.messages.setActivity(user_id=str(uid), type='typing')
-                time.sleep(2)
-                self.vkapi.messages.send(user_id=uid, message=message)
+                print messages
+                for message in messages:
+                    IN_QUEUE.put(message)
+                try:
+                    message = OUT_QUEUE.get()
+                except Queue.Empty:
+                    pass
+                else:
+                    time.sleep(0.5)
+                    mid = message['id']
+                    self.vkapi.markAsRead(message_ids=list(mid))
+                    uid = message['user_id']
+                    answer = message['body']
+                    time.sleep(0.5)
+                    self.vkapi.messages.setActivity(user_id=str(uid), type='typing')
+                    time.sleep(2)
+                    self.vkapi.messages.send(user_id=uid, message=answer)
+
+
+
+
+
 
 
 class IdError(Exception):
--- tests/VeraBot_tests.py
+++ tests/VeraBot_tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from nose.tools import *
 from VeraBot.VeraBot import *
 
@@ -19,9 +20,13 @@ def test_DB_thread():
 
 def test_VK_thread():
     pass
-
-print vkapi.messages.getDialogs(unread=1)['items']
+app_id = '4677976'
+user_name = 'genroe@mail.ru'
+password = raw_input('>')
+token = '8b3aa3c88b75ace274ee670cfe8223ea15b92d74de7ccebf80928e54ca82a6990ebbe33e8c8c1a3a8547f'
+vkapi = vk.API(app_id, user_name, password, timeout=1.5)
 vkapi.access_token = token
+#print vkapi.messages.getDialogs(unread=1)['items']
 vk_thread = Simle_VK_Thread(vkapi)
 db_thread = DB_Thread('test.db')
 c_bot = CBot()
@@ -29,15 +34,16 @@ def session():
     while True:
         try:
             message = IN_QUEUE.get()
+            print "SESSION GOT!"
         except Queue.Empty:
-            pass
+            print "IN EMPty"
         else:
-            mid = message['id']
-            uid = message['user_id']
-            message = c_bot.think(message)
-            print message
-            OUT_QUEUE.put((mid, uid, message))
+            question = message['body']
+            message['body'] = c_bot.think(message)
+            print "IN: %s Out: %s " % (question, message['body'])
+            OUT_QUEUE.put(message)
 ses = threading.Thread(target=session)
+ses2 = threading.Thread(target=session)
 ses.start()
 vk_thread.start()
 db_thread.start()
